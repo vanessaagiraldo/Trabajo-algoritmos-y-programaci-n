e70e3107bb88c87e8c961872a04521ef91348e89
Vanessa Giraldo <vanessaa.giraldo@udea.edu.co>
2025-11-27 20:14:02 -0500

    Agregamos el menú, pero la logica esta pendiente

--- src/main.py
+++ src/main.py
@@ -1,3 +1,49 @@
 # Este archivo tiene la función de mostrar el menú principal, 
 # llamar a las funciones correspondientes según la elección del usuario y 
 # manejar la lógica principal del programa, y controlar el flujo de la aplicación.
+
+
+# Por ahora solo creamos el menú principal.
+
+
+def menu_principal():
+    # Muestra el menú principal y permite al usuario seleccionar opciones.
+    while True:
+        print("\n=======================================")
+        print("     LA SIERRA RESORT NATURAL")
+        print("=======================================")
+        print("1. Planeación de la demanda")
+        print("2. Registro de clientes")
+        print("3. Cálculo de costos y ventas")
+        print("4. Módulo administrador")
+        print("5. Salir")
+        print("=======================================")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            print("\n → Módulo de planeación")
+            # Pendiente
+
+        elif opcion == "2":
+            print("\n → Registro de clientes")
+            # Pendiente
+
+        elif opcion == "3":
+            print("\n → Cálculo financiero")
+            # Pendiente
+
+        elif opcion == "4":
+            print("\n → Módulo administrador")
+            # Pendiente
+        elif opcion == "5":
+            print("\nSaliendo del sistema... ¡Hasta pronto!")
+            break
+
+        else:
+            print("\nERROR!!!!! \nOpción inválida. Intente nuevamente.")
+
+
+# Punto de entrada del programa
+if __name__ == "__main__":
+    menu_principal()
